b508d1197bfa954f66543dcb9123f7a68825bf12
Brent Howard <brent@mydynamics.io>
2018-08-15 16:55:12 +1000

    Added Test project
    Added Tests for Login, Logout and Get controller status

---
+++ UnifiClient/UnifiApiTests/ClientTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shouldly;
+using UnifiApi;
+using Xunit;
+
+namespace UnifiApiTests
+{
+    public class ClientTests
+    {
+        private const string _url = "https://demo.ubnt.com/";
+        private const string _user = "superadmin";
+        private const string _pass = "pq73KF59";
+
+        [Fact]
+        public async Task CanAuthenticate()
+        {
+            using (var unifiClient = new Client(_url))
+            {
+                var result = await unifiClient.LoginAsync(_user, _pass);
+                result.Result.ShouldBeTrue();
+                unifiClient.IsLoggedIn.ShouldBeTrue();
+            }
+        }
+        [Fact]
+        public async Task ShouldFailToAuthenticate()
+        {
+            using (var unifiClient = new Client(_url))
+            {
+                var result = await unifiClient.LoginAsync(_user, "");
+                result.Result.ShouldBeFalse();
+            }
+        }
+        [Fact]
+        public async Task CanLogout()
+        {
+            var unifiClient = new Client(_url);
+            await unifiClient.LoginAsync(_user, _pass);
+            unifiClient.IsLoggedIn.ShouldBeTrue();
+
+            await unifiClient.LogoutAsync();
+            unifiClient.IsLoggedIn.ShouldBeFalse();
+        }
+        [Fact]
+        public async Task ShouldGetControllerStatus()
+        {
+            using (var unifiClient = new Client(_url))
+            {
+                var loginResult = await unifiClient.LoginAsync(_user, _pass);
+                loginResult.Result.ShouldBeTrue();
+
+                var result = await unifiClient.GetControllerStatusAsync();
+                result.ShouldNotBeNull();
+                //TODO: Add proper checks
+            }
+        }
+    }
+}
