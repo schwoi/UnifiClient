d3c86fe24dc38379aa52bfef426039643cd707c7
Brent Howard <brent@mydynamics.io>
2018-08-15 16:57:24 +1000

    Added Guest methods and model

--- UnifiClient/UnifiApi/Client.cs
+++ UnifiClient/UnifiApi/Client.cs
@@ -118,6 +118,89 @@ namespace UnifiApi
             return records;
         }
 
+        #region Guest Methods
+        
+        /// <summary>
+        /// Authorize a guest client device.
+        /// </summary>
+        /// <param name="clientMac">The client MAC address.</param>
+        /// <param name="minutes">The minutes (from now) until authorization expires.</param>
+        /// <param name="uploadKbps">The upload limit KBPS.</param>
+        /// <param name="downloadKbps">The download limit KBPS.</param>
+        /// <param name="transferMb">The data transfer limit MB.</param>
+        /// <param name="accessPointMac">The access point mac.</param>
+        /// <returns>BoolResponse. true on success</returns>
+        public async Task<BoolResponse> AuthorizeGuestAsync(string clientMac, int minutes, int? uploadKbps = null, int? downloadKbps = null, int? transferMb = null, string accessPointMac = null)
+        {
+            var path = $"api/s/{Site}/cmd/stamgr";
+
+            var oJsonObject = new JObject();
+            oJsonObject.Add("cmd", "authorize-guest");
+            oJsonObject.Add("mac", clientMac);
+            oJsonObject.Add("minutes", minutes);
+            if (uploadKbps != null)
+                oJsonObject.Add("up", uploadKbps);
+            if (downloadKbps != null)
+                oJsonObject.Add("down", downloadKbps);
+            if (transferMb != null)
+                oJsonObject.Add("bytes", transferMb);
+            if (accessPointMac != null)
+                oJsonObject.Add("ap_mac", accessPointMac);
+
+            return await ExecuteBoolCommandAsync(path, oJsonObject);
+        }
+
+        /// <summary>
+        /// Unauthorize a guest client device.
+        /// </summary>
+        /// <param name="clientMac">The client MAC address.</param>
+        /// <returns>BoolResponse. true on success</returns>
+        public async Task<BoolResponse> UnauthorizeGuestAsync(string clientMac)
+        {
+            var path = $"api/s/{Site}/cmd/stamgr";
+
+            var oJsonObject = new JObject();
+            oJsonObject.Add("cmd", "unauthorize-guest");
+            oJsonObject.Add("mac", clientMac);
+
+            return await ExecuteBoolCommandAsync(path, oJsonObject);
+        }
+
+        /// <summary>
+        /// Extend guest validity
+        /// Unauthorize a guest client device.
+        /// </summary>
+        /// <param name="clientMac">The client MAC address.</param>
+        /// <returns>BoolResponse. true on success</returns>
+        public async Task<BoolResponse> ExtendGuest(string clientMac)
+        {
+            var path = $"api/s/{Site}/cmd/hotspot";
+
+            var oJsonObject = new JObject();
+            oJsonObject.Add("cmd", "extend");
+            oJsonObject.Add("_id", clientMac);
+
+            return await ExecuteBoolCommandAsync(path, oJsonObject);
+        }
+
+        public async Task<BaseResponse<Guest>> ListGuestsAsync(int within = 8760)
+        {
+            var path = $"api/s/{Site}/stat/guest";
+
+            var oJsonObject = new JObject();
+            oJsonObject.Add("within", within);
+
+            var response = await ExecuteJsonCommandAsync(path, oJsonObject);
+            
+            return JsonConvert.DeserializeObject<BaseResponse<Guest>>(response.Result);
+        }
+        #endregion
+
+        #region Commands
+
+
+
+
 
         private async Task<BoolResponse> ExecuteBoolCommandAsync(string path, JObject jsonData)
         {
@@ -190,5 +273,7 @@ namespace UnifiApi
 
             return returnResponse;
         }
+
+        #endregion
     }
 }
---
+++ UnifiClient/UnifiApi/Models/Guest.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+
+namespace UnifiApi.Models
+{
+    public class Guest
+    {
+        [JsonProperty(PropertyName = "_id")]
+        public string _id { get; set; }
+
+        [JsonProperty(PropertyName = "authorized_by")]
+        public string AuthorizedBy { get; set; }
+
+        [JsonProperty(PropertyName = "end")]
+        public int End { get; set; }
+
+        [JsonProperty(PropertyName = "expired")]
+        public bool Expired { get; set; }
+
+        [JsonProperty(PropertyName = "mac")]
+        public string Mac { get; set; }
+
+        [JsonProperty(PropertyName = "site_id")]
+        public string SiteId { get; set; }
+
+        [JsonProperty(PropertyName = "start")]
+        public int Start { get; set; }
+
+        [JsonProperty(PropertyName = "unauthorized_by")]
+        public string UnauthorizedBy { get; set; }
+
+        [JsonProperty(PropertyName = "ap_mac")]
+        public string ApMac { get; set; }
+
+        [JsonProperty(PropertyName = "bytes")]
+        public int? Bytes { get; set; }
+
+        [JsonProperty(PropertyName = "channel")]
+        public int? Channel { get; set; }
+
+        [JsonProperty(PropertyName = "duration")]
+        public int? Duration { get; set; }
+
+        [JsonProperty(PropertyName = "ip")]
+        public string Ip { get; set; }
+
+        [JsonProperty(PropertyName = "radio")]
+        public string Radio { get; set; }
+
+        [JsonProperty(PropertyName = "roam_count")]
+        public int? RoamCount { get; set; }
+
+        [JsonProperty(PropertyName = "rx_bytes")]
+        public int? RxBytes { get; set; }
+
+        [JsonProperty(PropertyName = "tx_bytes")]
+        public int? TxBytes { get; set; }
+        
+    }
+}
