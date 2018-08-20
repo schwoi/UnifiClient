d612e91d1324b57cb866fb8b520c67e42debe095
Brent Howard <brent@mydynamics.io>
2018-08-20 15:50:56 +1000

    Added methods for CreateUserGroup, DeleteUserGroup, UpdateUserGroup, AssignClientToUserGroup and ListUserGroups
    Added tests for the group methods
    Added Json Dependency to tests

---
+++ UnifiClient/UnifiApi/Models/UserGroup.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace UnifiApi.Models
+{
+    public class UserGroup
+    {
+        [JsonProperty(PropertyName = "_id")]
+        public string Id { get; set; }
+        [JsonProperty(PropertyName = "attr_hidden_id")]
+        public string AttrHiddenId { get; set; }
+
+        [JsonProperty(PropertyName = "attr_no_delete")]
+        public bool AttrNoDelete { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "qos_rate_max_down")]
+        public int QosRateMaxDown { get; set; }
+
+        [JsonProperty(PropertyName = "qos_rate_max_up")]
+        public int QosRateMaxUp { get; set; }
+
+        [JsonProperty(PropertyName = "site_id")]
+        public string SiteId { get; set; }
+        
+    }
+}
--- UnifiClient/UnifiApiTests/ClientTests.cs
+++ UnifiClient/UnifiApiTests/ClientTests.cs
@@ -11,10 +11,12 @@ namespace UnifiApiTests
 {
     public class ClientTests
     {
-        private const string _url = "https://demo.ubnt.com/";
-        private const string _user = "superadmin";
-        private const string _pass = "pq73KF59";
-
+        //private const string _url = "https://demo.ubnt.com/";
+        //private const string _user = "superadmin";
+        //private const string _pass = "pq73KF59";
+        private const string _url = "https://unifi.schwoi.com/";
+    private const string _pass = "bkCDbI4AXGt#NE";
+        private const string _user = "admin";
         [Fact]
         public async Task CanAuthenticate()
         {
@@ -222,6 +224,112 @@ namespace UnifiApiTests
                 checkNoteResult.Meta.Rc.ToLower().ShouldBe("ok");
                 checkNoteResult.Data.ShouldNotBeNull();
                 checkNoteResult.Data.First().Noted.ShouldBe(true);
+            }
+        }
+        [Fact]
+        public async Task ShouldBeAbleToCreateUserGroup()
+        {
+            using (var unifiClient = new Client(_url))
+            {
+                var loginResult = await unifiClient.LoginAsync(_user, _pass);
+                loginResult.Result.ShouldBeTrue();
+
+                var result = await unifiClient.CreateUserGroupAsync("Test Group", 100, 100);
+                result.Meta.Rc.ShouldBe("ok");
+                result.Data.Count.ShouldBeGreaterThanOrEqualTo(1);
+                result.Data.First().Id.ShouldNotBeEmpty();
+                result.Data.First().Name.ShouldBe("Test Group");
+                result.Data.First().QosRateMaxDown.ShouldBe(100);
+                result.Data.First().QosRateMaxUp.ShouldBe(100);
+
+                var deleteResult = await unifiClient.DeleteUserGroupAsync(result.Data.First().Id);
+            }
+        }
+        [Fact]
+        public async Task ShouldBeAbleToUpdateUserGroup()
+        {
+            using (var unifiClient = new Client(_url))
+            {
+                var loginResult = await unifiClient.LoginAsync(_user, _pass);
+                loginResult.Result.ShouldBeTrue();
+
+                var updateResult = await unifiClient.CreateUserGroupAsync("Test Group", 100, 100);
+                updateResult.Meta.Rc.ShouldBe("ok");
+                updateResult.Data.Count.ShouldBeGreaterThanOrEqualTo(1);
+                updateResult.Data.First().Id.ShouldNotBeEmpty();
+                updateResult.Data.First().Name.ShouldBe("Test Group");
+                updateResult.Data.First().QosRateMaxDown.ShouldBe(100);
+                updateResult.Data.First().QosRateMaxUp.ShouldBe(100);
+
+                var group = updateResult.Data.First();
+
+                var result = await unifiClient.UpdateUserGroupAsync(group.Id, group.SiteId, group.Name + " Updated", 50, 50);
+                result.Meta.Rc.ShouldBe("ok");
+                result.Data.Count.ShouldBeGreaterThanOrEqualTo(1);
+                result.Data.First().Id.ShouldNotBeEmpty();
+                result.Data.First().Name.ShouldBe("Test Group Updated");
+                result.Data.First().QosRateMaxDown.ShouldBe(50);
+                result.Data.First().QosRateMaxUp.ShouldBe(50);
+
+                var deleteResult = await unifiClient.DeleteUserGroupAsync(group.Id);
+            }
+        }
+
+        [Fact]
+        public async Task ShouldBeAbleToDeleteUserGroup()
+        {
+            using (var unifiClient = new Client(_url))
+            {
+                var loginResult = await unifiClient.LoginAsync(_user, _pass);
+                loginResult.Result.ShouldBeTrue();
+
+                var updateResult = await unifiClient.CreateUserGroupAsync("Test Group", 100, 100);
+                updateResult.Meta.Rc.ShouldBe("ok");
+                updateResult.Data.Count.ShouldBeGreaterThanOrEqualTo(1);
+               
+                var group = updateResult.Data.First();
+
+                var result = await unifiClient.DeleteUserGroupAsync(group.Id);
+                result.Result.ShouldBe(true);
+            }
+        }
+
+        [Fact]
+        public async Task ShouldBeAbleToAssignDeviceToUserGroup()
+        {
+            using (var unifiClient = new Client(_url))
+            {
+                var loginResult = await unifiClient.LoginAsync(_user, _pass);
+                loginResult.Result.ShouldBeTrue();
+
+                var updateResult = await unifiClient.CreateUserGroupAsync("Test Group", 100, 100);
+                updateResult.Meta.Rc.ShouldBe("ok");
+                updateResult.Data.Count.ShouldBeGreaterThanOrEqualTo(1);
+               
+                var group = updateResult.Data.First();
+
+                var clients = await unifiClient.ListOnlineClientsAsync();
+                var client = clients.Data.First();
+                var result = await unifiClient.AssignClientToUserGroupAsync(client.Id, group.Id);
+                result.Result.ShouldBe(true);
+
+                var deleteResult = await unifiClient.DeleteUserGroupAsync(group.Id);
+                deleteResult.Result.ShouldBe(true);
+            }
+        }
+
+        [Fact]
+        public async Task ShouldBeAbleToListUserGroups()
+        {
+            using (var unifiClient = new Client(_url))
+            {
+                var loginResult = await unifiClient.LoginAsync(_user, _pass);
+                loginResult.Result.ShouldBeTrue();
+
+                var result = await unifiClient.ListUserGroupsAsync();
+                result.Meta.Rc.ShouldBe("ok");
+                result.Data.Count.ShouldBeGreaterThanOrEqualTo(1);
+                result.Data.First().Id.ShouldNotBeEmpty();
             }
         }
     }
