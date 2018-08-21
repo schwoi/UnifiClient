8a1d6c470974176891e6d994e53635c8b26c001a
Brent Howard <brent@mydynamics.io>
2018-08-22 06:16:37 +1000

    Added Settings Model and file for configuring the tests

---
+++ UnifiClient/UnifiApiTests/Models/Settings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnifiApiTests.Models
+{
+    public class Settings
+    {
+        public string Url { get; set; }
+        public string User { get; set; }
+        public string Pass { get; set; }
+    }
+}
