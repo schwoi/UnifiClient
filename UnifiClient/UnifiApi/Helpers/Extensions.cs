cd7cfbd34ea2dbdd113863cbcff56cbb1bc1a5bd
Brent Howard <brent@mydynamics.io>
2018-08-24 21:29:26 +1000

    Added Minimum Version Attribute and Validation Extension

--- UnifiClient/UnifiApi/Helpers/Extensions.cs
+++ UnifiClient/UnifiApi/Helpers/Extensions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Reflection;
 using System.Text;
 
@@ -47,6 +49,37 @@ namespace UnifiApi.Helpers
 
             // Return the first if there was a match.
             return attribs.Length > 0 ? attribs[0].StringValue : null;
+        }
+
+        /// <summary>
+        /// Checks if the version supplied is valid againts the Methods Minimum Required Version Attribute.
+        /// Returns true if ... is valid.
+        /// </summary>
+        /// <param name="version">The version.</param>
+        /// <returns><c>true</c> if the specified version is valid; otherwise, <c>false</c>.</returns>
+        public static bool IsValid(this Version version)
+        {
+            StackTrace stackTrace = new StackTrace();
+            var requiredVersionMethod = stackTrace.GetFrame(3).GetMethod().GetCustomAttributes(typeof(MinimumVersionRequiredAttribute), false) as MinimumVersionRequiredAttribute[];
+
+            if (requiredVersionMethod == null || !requiredVersionMethod.Any())
+                return true;
+
+            var versionRequired = requiredVersionMethod.First();
+
+            if (versionRequired.MajorValue > version.Major)
+                return false;
+
+            if (versionRequired.MajorValue == version.Major && 
+                versionRequired.MinorValue > version.Minor)
+                return false;
+
+            if (versionRequired.MajorValue == version.Major && 
+                versionRequired.MinorValue == version.Minor && 
+                versionRequired.BuildValue > version.Build)
+                return false;
+
+            return true;
         }
     }
 }
---
+++ UnifiClient/UnifiApi/Helpers/MinimumVersionRequiredAttribute.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace UnifiApi.Helpers
+{
+
+    [AttributeUsage(AttributeTargets.Method)]
+    public class MinimumVersionRequiredAttribute : Attribute
+    {
+
+        #region Properties
+
+        /// <summary>
+        /// Holds the stringvalue for a value in an enum.
+        /// </summary>
+        public int MajorValue { get; protected set; }
+        public int MinorValue { get; protected set; }
+        public int BuildValue { get; protected set; }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Constructor used to init a MinimumVersionRequired Attribute
+        /// </summary>
+        /// <param name="value"></param>
+        public MinimumVersionRequiredAttribute(int major, int minor)
+        {
+            this.MajorValue = major;
+            this.MinorValue = minor;
+        }
+
+        public MinimumVersionRequiredAttribute(int major, int minor, int build)
+        {
+            this.MajorValue = major;
+            this.MinorValue = minor;
+            this.BuildValue = build;
+        }
+
+        public MinimumVersionRequiredAttribute(string version)
+        {
+            var requiredVersion = Version.Parse(version);
+            this.MajorValue = requiredVersion.Major;
+            this.MinorValue = requiredVersion.Minor;
+            this.BuildValue = requiredVersion.Build;
+        }
+        #endregion
+
+
+    }
+}
